a58cc85789467d3429d0e5ac0831523a6409c188
Patrick Rivera <patrick.x.rivera@gmail.com>
2025-01-01 16:51:02 -0500

    feat: basic twitter integration working nicely (#1)
    * feat: basic twitter integration working nicely
    
    * feat: add twitter posting

---
+++ scripts/twitter.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { Twitter } from '../src/environments/twitter';
+
+dotenv.config();
+
+async function testGetHomeTimeline() {
+  try {
+    const twitterEnv = new Twitter();
+
+    const timeline = await twitterEnv.post('"posting from the void"');
+
+    console.log(timeline);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Run the test
+testGetHomeTimeline()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error('Fatal error:', error);
+    process.exit(1);
+  });
